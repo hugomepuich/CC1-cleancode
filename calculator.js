9137111e9b4476586859785a23455e18cbca797f
hugomepuich <hugo@MacBook-Pro-de-Hugo-2.local>
2024-11-28 12:46:44 +0100

    bonus code

--- calculator.js
+++ calculator.js
@@ -12,7 +12,7 @@ function hasNMatching(rolls, x) {
 
 function MatchingDices(rolls) {
     rolls_count = {};
-    
+
     rolls.forEach((roll, index) => {
         if (rolls_count[roll] === undefined) {
             rolls_count[roll] = 1;
@@ -37,41 +37,72 @@ function Yams(rolls) {
     return rolls.filter((val) => val == rolls[0]).length == 5;
 }
 
-function calculatePoints(rolls) {
+function calculatePoints(rolls, skip_to_sum = false) {
 
+    if (!skip_to_sum) {
+        if (hasNMatching(rolls, 5)) {
+            return { "points": 50, 'figure': 'Y' };
+        }
 
-    if (hasNMatching(rolls, 5)) {
-        return 50;
+        if (GrandSuite(rolls) && rolls.length == 5) {
+            return { "points": 40, 'figure': 'G' }
+        }
+
+        // Full
+        if (hasNMatching(rolls, 3) && hasNMatching(rolls, 2)) {
+            return { "points": 30, 'figure': 'F' }
+        }
+
+        // Carré
+        if (hasNMatching(rolls, 4)) {
+            return { "points": 35, 'figure': 'C' }
+        }
+
+        // Brelan
+        if (hasNMatching(rolls, 3)) {
+            return { "points": 28, 'figure': 'B' }
+        }
     }
-
-    if (GrandSuite(rolls) && rolls.length == 5) {
-        return 40;
-    }
-
-    // Full
-    if (hasNMatching(rolls, 3) && hasNMatching(rolls, 2)) {
-        return 30;
-    }
-
-    // Brelan
-    if (hasNMatching(rolls, 3)) {
-        return 28;
-    }
-
-    // Carré
-    if (hasNMatching(rolls, 4)) {
-        return 35;
-    }
-
-
     const sum = rolls.reduce((a, b) => a + b, 0);
-    return sum;
+    return { "points": sum, 'figure': undefined };
 }
 
+// J'crois qu'il y avait pas besoin de ça en fait j'ai mal compris tant pis
 function CalculatePoints(rolls) {
-    const sum = rolls.reduce((a, b) => a + b, 0);
-    const pts = calculatePoints(rolls);
-    return sum > pts ? sum : pts;
+    if (Array.isArray(rolls)) {
+        if (Array.isArray(rolls[0]) == Array) {
+            const points = [];
+            let figures = {
+                'B': 0,
+                'C': 0,
+                'F': 0,
+                'G': 0,
+                'Y': 0
+            }
+
+            rolls.forEach((roll) => {
+                const res = calculatePoints(roll);
+                if (res.figure != undefined) {
+                    const figure = res.figure;
+                    if (figures[figure] == 0) {
+                        figures[figure]++;
+                        points += res.points;
+                    }
+                    else {
+                        points += calculatePoints(roll, skip_to_sum=true)
+                    }
+                }
+                else {
+                    points += res.points;
+                }
+            })
+            return points;
+        }
+        const pts = calculatePoints(rolls);
+        console.log(pts);
+        return pts.points;
+    }
+    return 0;
 }
 
 exports.calc = CalculatePoints;
