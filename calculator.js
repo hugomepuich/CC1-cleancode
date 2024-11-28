5fed54b150d542294e6523cf37296fd0afc60b52
hugomepuich <hugo@MacBook-Pro-de-Hugo-2.local>
2024-11-28 11:58:03 +0100

    Added Matching and some tests

--- calculator.js
+++ calculator.js
@@ -1,6 +1,21 @@
+function MatchingDices(rolls) {
+    rolls_count = {};
+    
+    rolls.forEach((roll, index) => {
+        if (rolls_count[roll] === undefined) {
+            rolls_count[roll] = 1;
+        } else {
+            rolls_count[roll]++;
+        }
+    });
+
+    return rolls_count;
+}
+
 function CalculatePoints(rolls) {
     const sum = rolls.reduce((a, b) => a + b, 0);
     return sum;
 }
 
 exports.calc = CalculatePoints;
+exports.matching = MatchingDices;
--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,17 @@
 const calculator = require('./calculator');
 
 test('Voir si la somme fonctionne bien', () => {
+    expect(calculator.calc([1])).toBe(1);
+    expect(calculator.calc([1,2])).toBe(3);
+    expect(calculator.calc([1,2,3])).toBe(6);
     expect(calculator.calc([1,2,3,4,5,6])).toBe(21);
+})
+
+test('Voir si le matching fonctionne', () => {
+    expect(calculator.matching([1, 2, 1, 3, 5])['1'])
+    .toBe(2);
+    expect(calculator.matching([1, 2, 1, 3, 5])['2'])
+    .toBe(1);
+    expect(calculator.matching([1, 2, 1, 3, 5])['6'])
+    .toBe(undefined);
 })
